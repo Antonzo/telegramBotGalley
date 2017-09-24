916e169aeef7e635af79a874c6979600895b86df
Sergey <kaptch@gmail.com>
2017-09-24 11:47:45 +0300

    first upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+import config
+import telebot
+import time
+
+bot = telebot.TeleBot(config.token)
+
+audio = open('/home/kaptch/Downloads/oceanman.mp3', 'rb')
+
+prevOceanman = time.time()
+
+
+@bot.message_handler(commands=['oceanman'])
+def answer_all_msg(message):
+    global prevOceanman
+    if time.time() - prevOceanman > 10:
+        bot.send_audio(message.chat.id, audio)
+        prevOceanman = time.time()
+
+
+@bot.message_handler(commands=['help'])
+def answer_all_msg(message):
+    bot.send_message(message.chat.id, '/help - справка\n'
+                                      '/oceanman - отправить оушенмена\n'
+                                      '/reported - пойти к чёрту\n')
+
+
+@bot.message_handler(commands=['reported'])
+def answer_all_msg(message):
+    bot.send_message(message.chat.id, ('Иди к чёрту @' + message.user.username))
+    bot.delete_message(message.chat.id, message)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,3 @@
+
+
+token = '370476838:AAFqHAocJuD_u0YSV84c4EfJj62KKJXa3dA'
