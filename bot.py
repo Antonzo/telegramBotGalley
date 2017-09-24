81d1b83dc81e7489bc7714f6edc703468978ab0e
Kaptch <kaptch@gmail.com>
2017-09-24 12:12:24 +0300

    1.0

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import time
 
 bot = telebot.TeleBot(config.token)
 
-audio = open('/home/kaptch/Downloads/oceanman.mp3', 'rb')
+audio = open('./assets/oceanman.mp3', 'rb')
 
 prevOceanman = time.time()
 
 
 @bot.message_handler(commands=['oceanman'])
-def answer_all_msg(message):
+def oceanman_msg(message):
     global prevOceanman
     if time.time() - prevOceanman > 10:
         bot.send_audio(message.chat.id, audio)
@@ -18,17 +18,21 @@ def answer_all_msg(message):
 
 
 @bot.message_handler(commands=['help'])
-def answer_all_msg(message):
+def help_msg(message):
     bot.send_message(message.chat.id, '/help - справка\n'
                                       '/oceanman - отправить оушенмена\n'
                                       '/reported - нет\n')
 
 
 @bot.message_handler(commands=['reported'])
-def answer_all_msg(message):
+def reported_msg(message):
     bot.send_message(message.chat.id, ('Нет @' + message.user.username))
     bot.delete_message(message.chat.id, message)
 
+@bot.message_handler(commands=['sendreported'])
+def send_reported_msg(message):
+    bot.send_message(message.chat.id, ('/reported'))
+    bot.delete_message(message.chat.id, message)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
